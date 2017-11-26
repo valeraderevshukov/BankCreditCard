6c7bc3898649aa132678ba68668c8d911756f8de
Валерий <kaktuz34@gmail.com>
2017-11-26 21:09:56 +0200

    desctop, tablet done

---
+++ src/js/components/_nav.js
@@ -0,0 +1,37 @@
+import { BODY, OPEN, ACTIVE } from '../_constants';
+import { SCROLL_TO } from '../_utils';
+
+(() => {
+
+  const $btn = $('.js-nav-btn');
+  const $nav = $('.js-nav');
+  const $links = $('.js-nav-link');
+  const $sections = $('[data-section]');
+  const header = '.js-header';
+
+  //SCROLL TO
+  $links.click(function(e) {
+    e.preventDefault();
+
+    const target = $(this).attr('href');
+    const position = $sections
+      .filter(`[data-section="${target}"]`)
+      .offset().top;
+
+    SCROLL_TO(position);
+  });
+
+  $btn.click(e => {
+    e.preventDefault();
+    $nav.toggleClass(OPEN);
+    $btn.toggleClass(ACTIVE);
+  });
+
+  BODY.click(e => {
+    const $target = $(e.target);
+    if ($target.closest(header).length) return;
+    $nav.removeClass(OPEN);
+    $btn.removeClass(ACTIVE);
+  });
+
+})();
